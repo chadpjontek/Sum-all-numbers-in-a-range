0039abd220fc3378281f81e068533c3c1f3016a2
Chad Pjontek <chadpjontek@gmail.com>
2017-10-16 04:08:25 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sum-all-numbers-in-a-range
+Finds the sum of all whole numbers within a range. Accepts an array of two numbers (order doesn't matter).
---
+++ Sum-all-numbers-in-a-range.js
@@ -0,0 +1,20 @@
+function sumAll(arr) {
+    //find highest number in array
+    var max = arr.reduce(function (a, b) {
+        return Math.max(a, b);
+    });
+    //find lowest number in array
+    var min = arr.reduce(function (a, b) {
+        return Math.min(a, b);
+    });
+    //add each number in between lowest and highest number to array
+    for (var i = min + 1; i < max; i++) {
+        arr.push(i);
+    }
+    //add all values of array together
+    var result = arr.reduce(function (total, currentValue) {
+        return total + currentValue;
+    });
+    return result;
+}
+sumAll([1, 4]);
